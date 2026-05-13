8698a074535534709042ecc3cbbad27629c1a989
sohee52 <wneh2023@naver.com>
2026-05-14 01:19:17 +0900

    [Chore] #28 Windows 호환 prepare 스크립트로 변경

---
+++ scripts/setup-githooks.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const { execFileSync } = require("child_process");
+
+const rootDir = path.resolve(__dirname, "..");
+const gitDir = path.join(rootDir, ".git");
+const hooksDir = path.join(rootDir, ".githooks");
+const commitMsgHook = path.join(hooksDir, "commit-msg");
+
+try {
+  if (!fs.existsSync(gitDir)) {
+    process.exit(0);
+  }
+
+  execFileSync("git", ["config", "core.hooksPath", ".githooks"], {
+    cwd: rootDir,
+    stdio: "ignore",
+  });
+
+  if (process.platform !== "win32" && fs.existsSync(commitMsgHook)) {
+    fs.chmodSync(commitMsgHook, 0o755);
+  }
+} catch (error) {
+  console.warn(`Skipping git hook setup: ${error.message}`);
+}
